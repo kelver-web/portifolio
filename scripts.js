4f8fed0941f5dfeef991957b8c655c63442f1d3a
kelver <kelverwt@gmail.com>
2023-05-02 20:57:54 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// let anchorSelector = 'a[href^="#"]';
+// let anchorList = document.querySelectorAll(anchorSelector);
+
+
+// anchorList.forEach((link) => {
+//   link.onclick = function (e) {
+//     e.preventDefault();
+
+//     let destination = document.querySelector(this.hash);
+
+//     destination.scrollIntoView({
+//       behavior: "smooth",
+//     });
+//   };
+// });
